011379571325072ba517ae869922d2c688f6c721
angelicatrejoarrieta61-bit <angelicatrejoarrieta61@gmail.com>
2026-07-26 20:23:09 -0600

    Fix formatting in config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.APP_CONFIG = {
   SUPABASE_URL: "https://fwguqporpryhnokrsuva.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3Z3VxcG9ycHJ5aG5va3JzdXZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4NTc3MjIsImV4cCI6MjEwMDQzMzcyMn0.LOJ7A3SbWlKbujDlqRDmkz6GeP56neyWMQna2TWLcM0",
   EDITOR_EMAIL: "joe@playmedia.com.mx",
-  EDITOR_PASSWORD: "An239901",
+  EDITOR_PASSWORD: "An239901"
 };
